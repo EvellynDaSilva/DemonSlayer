75b8618887218f6e04ef762efacdd0c702329bf1
Evellyn <evellyn.d.s.d@gmail.com>
2023-05-18 16:56:07 -0300

    ;;;

--- js/card.js
+++ js/card.js
@@ -1,7 +1,30 @@
-export const card = async() => {
-    const url = `{https://demon-slayer-api.onrender.com/v1/`
-    const response = await fetch(url)
-    const card = await response.json()
+'use strict'
 
-    return card
+const personagens = () => {
+    alert ("Oieee")
 }
+
+
+
+document.getElementById('image').addEventListener('click', personagens)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export const card = async() => {
+//     const url = `{https://demon-slayer-api.onrender.com/v1/`
+//     const response = await fetch(url)
+//     const card = await response.json()
+
+//     return card
+// }
--- js/character.js
+++ js/character.js
@@ -1,4 +1,27 @@
 'use strict'
+const cards = document.querySelector('.cards')
 
-const card = document.getElementById('card').value
-const url = `https://demon-slayer-api.onrender.com/v1`
+// fetch('https://demon-slayer-api.onrender.com/v1')
+
+ //Criando um card padrão
+ const card = document.createElement('div') 
+ card.classList.add('card')
+ console.log(card)
+
+//Colocando cada card dentro da div cards
+cards.appendChild(card)
+
+
+
+
+
+ //Preenchendo os dados de cada personagem
+
+
+
+
+
+
+
+// const card = document.getElementById('card').value
+// const url = `https://demon-slayer-api.onrender.com/v1`
