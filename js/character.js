90697a823b4bfec9c9fc3c4b0753ee11dea725e8
Evellyn <evellyn.d.s.d@gmail.com>
2023-05-25 16:58:10 -0300

    .....

--- js/character.js
+++ js/character.js
@@ -1,30 +1,65 @@
-'use strict'
+const getPersonagens = async () => {
+
+    const url = `https://demon-slayer-api.onrender.com/v1`
+    const response = await fetch(url)
+    const dado = await response.json()
+    return dado
+}
+const personagens = await getPersonagens()
+console.log(personagens)
+
+
+ const carregarCard = async () => {
+    const card = document.getElementById('cards')
+    const cardsJSON = await personagens.map(criarCard)
+
+    card.replaceChildren(...cardsJSON)
+ }
+
+ const criarCard = (personagens) => {
+    console.log(personagens.image)
+
+    const card = document.createElement('div')
+        card.classList.add('card')
+
+        const imagem = document.createElement('img')
+        imagem.classList.add('character')
+        imagem.src = personagens.image
+        card.append(imagem)
+        return card
+ }
+
+ carregarCard ()
 
 const cards = document.querySelector('.cards')
 
-fetch('https://demon-slayer-api.onrender.com/v1')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        data.forEach(personagem  => {
-            console.log(personagem)
-            console.log(personagem.name)
+// fetch('https://demon-slayer-api.onrender.com/v1')
+//     .then((response) => response.json())
+//     .then((data) => {
+//         console.log(data)
+//         data.forEach(personagem  => {
+//             console.log(personagem)
+//             console.log(personagem.name)
+//             console.log(personagem.image)
 
-        })
+
+//         })
+        
+     
+//     })
+
+
         //Criando um card padrão
-        const card = document.createElement('div')
-        card.classList.add('card')
-        console.log(card)
-
+        
+      
+    
          //Colocando cada card dentro da div cards
-        cards.appendChild(card)
-
+        
+    
          //Preenchendo os dados de cada personagem
-        card.innerHTML = `
-        <a href="/nomedospersonagens" onclick="route()" class="link-image"></a>
-        <img class="character" id ="image" src="./img/${personagem.image} " alt="">`
-    })
-
+       
+    
+   
 
 // // const card = document.getElementById('card').value
 // // const url = `https://demon-slayer-api.onrender.com/v1`
