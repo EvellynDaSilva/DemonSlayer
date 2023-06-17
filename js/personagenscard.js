de2d82cb2840c832101e54f8aa3ba3ae300ab4b2
evellynDaSilva <evellyn.d.s.d@gmail.com>
2023-06-16 21:38:25 -0300

    lalalala

--- js/personagenscard.js
+++ js/personagenscard.js
@@ -1,6 +1,53 @@
 'use strict'
 
-
+const getPersonagens = async () => {
+    const url = `https://demon-slayer-api.onrender.com/v1`;
+    const response = await fetch(url);
+    const dado = await response.json();
+    return dado;
+  };
+  
+  const personagens = await getPersonagens();
+  console.log(personagens);
+  
+  const carregarCard = async () => {
+    const card = document.getElementById("cards");
+    const cardsJSON = await personagens.map(criarCard);
+  
+    card.replaceChildren(...cardsJSON);
+  };
+  // styles() {
+  //     const css = document.createElement('style')
+  //     css.textContent =`
+  
+  //     *{
+  //         margin: 0;
+  //         padding: 0;
+  //         box-sizing: border-box;
+  //     }
+  
+  //     `
+  // }
+  
+  const criarCard = (personagens) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.style.backgroundImage = `url(${personagens.image})`;
+  
+    //   imagem.src = personagens.image;
+  //   imagem.setAttribute("src", personagens.image);
+  
+    const nameCharacter = document.createElement("p");
+    nameCharacter.classList.add("nome_character");
+    nameCharacter.textContent = personagens.name;
+    card.append(nameCharacter); 
+  
+    return card;
+  };
+  
+  carregarCard();
+  
+  const cards = document.querySelector(".cards");
 
 
 
